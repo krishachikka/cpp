7a5d60845a7c18fa4b4b58bfbb5b7c578842cc2a
Krisha <chikkakrisha@gmail.com>
2025-01-28 22:55:54 +0530

    done with dfs

--- bfs.cpp
+++ bfs.cpp
@@ -27,6 +27,28 @@ vector<int> bfs(int v, vector<int> adj[]){
     return bfs;
 }
 
+//recursive function for dfs
+// we added &dfs_seq to avoid creating new/multiple vector
+void dfs_helper(int node, vector<int> adj[], int visited[], vector<int>& dfs_seq) {
+    visited[node] = 1;
+    dfs_seq.push_back(node);
+
+    // going to the depth of adj node
+    for (auto it : adj[node]) {
+        if (!visited[it]) {
+            dfs_helper(it, adj, visited, dfs_seq);
+        }
+    }
+}
+
+vector<int> dfs(int v, vector<int> adj[]) {
+    int visited[v] = {0};
+    vector<int> dfs_seq;
+    // 0 = start node
+    dfs_helper(0, adj, visited, dfs_seq);
+    return dfs_seq;
+}
+
 int main() {
     int v = 5; 
     vector<int> adj[v]; 
@@ -41,12 +63,20 @@ int main() {
     adj[3].push_back(1);
     adj[4].push_back(1);
 
-    vector<int> result = bfs(v, adj);
+    vector<int> bfs_result = bfs(v, adj);
+    vector<int> dfs_result = dfs(v, adj);
 
     cout << "BFS Traversal: ";
-    for (int i : result) {
+    for (int i : bfs_result) {
         cout << i << " ";
     }
+    cout << endl;
+
+    cout << "DFS Traversal: ";
+    for (int i : dfs_result) {
+        cout << i << " ";
+    }
+    cout << endl;
 
     return 0;
 }
